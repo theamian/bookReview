c3c56a75b0f942563fd27027f7c5c834928da07e
theamian <damijan.nejasmic@gmail.com>
2020-06-23 15:22:12 -0700

    Add register page with forms

--- application.py
+++ application.py
@@ -24,3 +24,7 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
